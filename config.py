1884a370921735b5dc480666641d598fe11d0abb
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2022-11-21 09:01:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  12168140 #Tu api id de telegram
 API_HASH = '3504ce0eddb7dff4288d05d5e3dc5e4c' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','Ghosthell0210').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','Hiyabo').split(';')
 
 static_proxy = 'http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
 PROXY = ProxyCloud.parse(static_proxy)
